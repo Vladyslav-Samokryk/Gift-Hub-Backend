a025bf3e5f7235b11ae2496c4ce74bcc7c92f006
Vladyslav Samokryk <Vladyslav Samokryk@LAPTOP-T85FR3GP>
2023-07-24 11:13:26 +0300

    Initial commit

---
+++ src/main/java/com/gifthub/gifthubbackend/GiftHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gifthub.gifthubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftHubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftHubBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gifthub/gifthubbackend/GiftHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gifthub.gifthubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftHubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
